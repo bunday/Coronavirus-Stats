61957f1b7042ec9c5529731fda08e28d67c92444
Zadat Olayinka <bundayy@outlook.com>
2020-04-29 17:43:15 +0100

    conditional render

--- src/components/Global.js
+++ src/components/Global.js
@@ -3,7 +3,7 @@ import Stat from "./Stat";
 
 function GlobalStat() {
   const [summary, setSummary] = useState({});
-  const [loading, setLoading] = useState({});
+  const [loading, setLoading] = useState(true);
   // useEffect(() => {
   //     fetch('https://api.covid19api.com/summary')
   //     .then(res => res.json())
@@ -15,31 +15,44 @@ function GlobalStat() {
   // }, [])
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+  function renderPage() {
+    if (loading) {
+      return (
+        <div className="text-white text-center">
+          <p>Please wait while we fetch the latest data...</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="text-white">
+          <div className="py-8">
+            <div className="flex justify-center py-2">
+              <p className="text-2xl"> Global Statistics</p>
+            </div>
+            <div className="flex sm:flex-row sm:justify-between flex-col">
+              <Stat title="Total Confirmed" count={numberWithCommas(3000)} />
+              <Stat title="Total Deaths" count={numberWithCommas(3000)} />
+              <Stat title="Total Recovered" count={numberWithCommas(3000)} />
+            </div>
+          </div>
+          <div className="py-8">
+            <div className="flex justify-center py-2">
+              <p className="text-2xl"> Nigeria's Statistics</p>
+            </div>
+            <div className="flex sm:flex-row sm:justify-between flex-col">
+              <Stat title="Total Confirmed" count={numberWithCommas(3000)} />
+              <Stat title="Total Deaths" count={numberWithCommas(3000)} />
+              <Stat title="Total Recovered" count={numberWithCommas(3000)} />
+            </div>
+          </div>
+        </div>
+      );
     }
+  }
   return (
-    <div className="text-white">
-      <div className="py-8">
-        <div className="flex justify-center py-2">
-          <p className="text-2xl"> Global Statistics</p>
-        </div>
-        <div className="flex sm:flex-row sm:justify-between flex-col">
-          <Stat title="Total Confirmed" count={numberWithCommas(3000)} />
-          <Stat title="Total Deaths" count={numberWithCommas(3000)} />
-          <Stat title="Total Recovered" count={numberWithCommas(3000)} />
-        </div>
-      </div>
-      <div className="py-8">
-      <div className="flex justify-center py-2">
-        <p className="text-2xl"> Nigeria's Statistics</p>
-      </div>
-      <div className="flex sm:flex-row sm:justify-between flex-col">
-        <Stat title="Total Confirmed" count={numberWithCommas(3000)} />
-        <Stat title="Total Deaths" count={numberWithCommas(3000)} />
-        <Stat title="Total Recovered" count={numberWithCommas(3000)} />
-      </div>
-      </div>
-    </div>
-  );
+      renderPage()
+  )
 }
 
 export default GlobalStat;
